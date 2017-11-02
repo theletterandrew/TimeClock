03f92ce0d7362397fcabd9a5740801f8f55d4917
Andrew Phillips <andrew.austen.phillips@gmail.com>
2017-11-02 09:45:28 -0500

    added a start
    nothing too serious, just playing around and wondering how I'm going to write this thing

---
+++ time.py
@@ -0,0 +1,6 @@
+response = input("Enter a command. Type HELP for list of options: ")
+
+if response == "HELP":
+	print("First type your username")
+	print("Then type the project name")
+	print("Then type either START or STOP")
