8a8a2c0635483a3571e2932c1b5cfd6994fd0002
Andrew Phillips <andrewaustenp@gmail.com>
2017-11-02 16:03:20 -0500

    No longer going to be in Python
    Deleted time.py because I'm no longer going to be making this app in Python.

--- time.py
+++
@@ -1,6 +0,0 @@
-response = input("Enter a command. Type HELP for list of options: ")
-
-if response == "HELP":
-	print("First type your username")
-	print("Then type the project name")
-	print("Then type either START or STOP")
